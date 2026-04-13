41dd4cb799c538c9adfd89a07fe1d5e775c9ca3a
JakeMarchetti <marchj6@rpi.edu>
2026-04-13 14:43:39 -0400

    final lab8 commit

--- projectsite/labInfo/lab8/lab8.js
+++ projectsite/labInfo/lab8/lab8.js
@@ -30,6 +30,49 @@ $(document).ready(function() {
         }
     });
 
+    $.ajax({
+        type: "GET",
+        url: "labInfo/lab8/lab8.json",
+        dataType: "json",
+        success: function (data) {
+            var output = "<item>";
+
+            $.each(data.rss, function(index, entry){
+                output += "<title>" + entry.title + "</title>";
+                output += "<description>" + entry.description + "</description>";
+                output += "<link>" + entry.link + "</link>";
+                output += "<guid>" + entry.guid + "</guid>";
+                output += "<pubDate>" + entry.pubDate + "</pubDate>";
+                output += "</item>";
+            });
+            $("#rssFeed").html(output);
+            },
+            error: function () {
+                $("#rssFeed").html("<p>Sorry, the RSS feed could not be loaded.</p>");
+            }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "labInfo/lab8/lab8.json",
+        dataType: "json",
+        success: function (data) {
+            var output = "<entry>";
+        $.each(data.atom, function(index, entry){
+                output += "<title>" + entry.title + "</title>";
+                output += "<link>" + entry.link + "</link>";
+                output += "<updated>" + entry.updated + "</updated>";
+                output += "<summary>" + entry.summary + "</summary>";
+                output += "</entry>";
+            });
+            $("#rssItems").html(output);
+            },
+            error: function () {
+                $("#rssItems").html("<p>Sorry, the Atom feed could not be loaded.</p>");
+            }
+    })
+
+
     $("#toggleLabs").click(function() {
         $(".lab-display").slideToggle(500);
 
